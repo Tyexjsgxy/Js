3aec9fcc8518e6ce8b625410fd68d7ac7a6af236
Eduardo Cruz <eduardocostadacruz11@gmail.com>
2025-06-17 21:11:04 +0100

    Number generator addded

---
+++ NumGen.js
@@ -0,0 +1,13 @@
+const num1 = Math.floor(Math.random() * 100) + 10;
+const num2 = Math.floor(Math.random() * 100) + 10;
+const num3 = Math.floor(Math.random() * 100) + 10;
+const num4 = Math.floor(Math.random() * 100) + 10;
+const num5 = Math.floor(Math.random() * 100) + 10;
+const num6 = Math.floor(Math.random() * 100) + 10;
+const num7 = Math.floor(Math.random() * 100) + 10;
+const num8 = Math.floor(Math.random() * 100) + 10;
+
+
+const result = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num1 * num2 * num3 
+const result2 = result * Math.floor(Math.random() * 10) + 1;
+console.log(result2)
--- average.js
+++ average.js
@@ -1,5 +1,6 @@
 let num1 = 2;
 let num2 = 3;
+let num3 = 4;
 
-let avarage = (num1 + num2) / 2;
+let avarage = (num1 + num2 + num3) / 2;
 console.log("The avarage is: " + avarage);
