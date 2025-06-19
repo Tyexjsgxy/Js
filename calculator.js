45e1748eaa3facef0157b59170b3f4d392b945eb
Eduardo Cruz <eduardocostadacruz11@gmail.com>
2025-06-19 20:48:46 +0100

    calculator

---
+++ calculator.js
@@ -0,0 +1,27 @@
+let number1 = 2;
+let number2 = 3;
+let operator = '/';
+
+
+
+
+let result = '';
+if (operator === '+') {
+  let result = number1 + number2;
+console.log(result)
+
+}
+
+
+if (operator === '-') {
+  let result = number1 - number2;
+console.log(result)
+
+}
+
+
+if (operator === '/') {
+  let result = number1 / number2;
+console.log(result)
+
+}
